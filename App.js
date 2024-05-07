eca501ebd6eb4837c6cf4b80011ea8f85ae26775
DragonOfD00M <martin.grytter@gmail.com>
2024-05-07 14:51:40 +0200

    First Version

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { View, StyleSheet, Text, Button } from "react-native";
 
 export default function App() {
+  function onPressLearnMore() {
+    return alert("Hello, World!");
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ height: 20 }} />
+      <Text style={styles.titleStyle}>Hello</Text>
+      <View style={(styles.menuButtonStyle, { alignItems: "right" })}>
+        <Button
+          onPress={onPressLearnMore}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    justifyContent: "top",
+  },
+  titleStyle: {
+    color: "#000",
+    fontSize: 20,
+  },
+  menuButtonStyle: {
+    height: 30,
+    width: 30,
   },
 });
